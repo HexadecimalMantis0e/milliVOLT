dc9166083eba4155fda084a6d18593da004dbe6e
HexadecimalMantis0e <32582986+HexadecimalMantis0e@users.noreply.github.com>
2021-12-27 17:50:58 -0500

    Fix variable scopes

--- milliVOLT.c
+++ milliVOLT.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int i;
-int nameSize;
-unsigned int magic;
-unsigned int numOfFiles;
-unsigned int nameSecSize;
-unsigned int nameOffset;
-unsigned int getBack;
-unsigned int fileAddress;
-unsigned int fileSize;
-unsigned char nameChar;
-char *nameBuffer;
-char *fileBuffer;
-FILE *f0;
-FILE *f1;
-
 int main(int argc, char **argv) {
 
     if (argc != 2 ) {
@@ -23,13 +8,14 @@ int main(int argc, char **argv) {
         return(1);
     }
 
-    f0 = fopen(argv[1], "rb");
+    FILE *f0 = fopen(argv[1], "rb");
 
     if (f0 == NULL) {
-        printf("Unable to open VOLT file!\n");
+        printf("Unable to open VOLT archive!\n");
         return(1);
     }
 
+    unsigned int magic;
     fread(&magic, sizeof(unsigned int), 1, f0);
 
     if (magic != 0x544C4F56 ) {
@@ -38,40 +24,45 @@ int main(int argc, char **argv) {
     }
 
     fseek(f0, 0x04, SEEK_CUR);
+    unsigned int numOfFiles;
     fread(&numOfFiles, sizeof(unsigned int), 1, f0);
     printf("Number of files: 0x%x\n", numOfFiles);
+    unsigned int nameSecSize;
     fread(&nameSecSize, sizeof(unsigned int), 1, f0);
     printf("Name section size: 0x%x\n", nameSecSize);
 
-    for (i = 0; i < numOfFiles; i++) {
+    for (int i = 0; i < numOfFiles; i++) {
         fseek(f0, 0x08, SEEK_CUR);
+        unsigned int nameOffset;
         fread(&nameOffset, sizeof(unsigned int), 1, f0);
         printf("Name offset: 0x%x\n", nameOffset);
-        getBack = ftell(f0);
+        unsigned int getBack = ftell(f0);
         nameOffset += numOfFiles * 0x0C + 0x10;
         fseek(f0, nameOffset, SEEK_SET);
+        unsigned int fileAddress;
         fread(&fileAddress, sizeof(unsigned int), 1, f0);
         printf("File address: 0x%x\n", fileAddress);
         fseek(f0, 0x04, SEEK_CUR);
+        unsigned int fileSize;
         fread(&fileSize, sizeof(unsigned int), 1, f0);
         printf("File size: 0x%x\n", fileSize);
         fseek(f0, 0x04, SEEK_CUR);
-        nameChar = ' ';
+        unsigned char nameChar = ' ';
+        int nameSize = 0;
 
         while (nameChar != 0x00) {
             fread(&nameChar, sizeof(unsigned char), 1, f0);
             nameSize++;
         }
 
-        nameBuffer = (char *) malloc(sizeof(char) * nameSize);
+        char *nameBuffer = (char *) malloc(sizeof(char) * nameSize);
         fseek(f0, -nameSize, SEEK_CUR);
         fread(nameBuffer, sizeof(char), nameSize, f0);
         printf("Name: %s\n", nameBuffer);
-        nameSize = 0;
-        f1 = fopen(nameBuffer, "wb");
+        FILE *f1 = fopen(nameBuffer, "wb");
         free(nameBuffer);
         fseek(f0, fileAddress, SEEK_SET);
-        fileBuffer = (char *) malloc(sizeof(char) * fileSize);
+        char *fileBuffer = (char *) malloc(sizeof(char) * fileSize);
         fread(fileBuffer, sizeof(char), fileSize, f0);
         fwrite(fileBuffer, sizeof(char), fileSize, f1);
         free(fileBuffer);
